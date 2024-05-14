93a15eff62aa839928bc2ec907a056d7bb2d80f8
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-05-14 10:06:58 -0500

    challenge 3 completed

---
+++ challenge3.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'json'
+require 'pp'
+ 
+url = 'https://dog.ceo/api/breeds/list/all'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
+pp dog_breeds # pp stands for pretty print.
+
+# Access the hash containing the breeds and sub-breeds
+breeds_hash = dog_breeds['message']
+
+# Print out the dog breeds and sub-breeds
+breeds_hash.each do |breed, sub_breeds|
+  puts "* #{breed.capitalize}"
+  sub_breeds.each do |sub_breed|
+    puts "  * #{sub_breed.capitalize}" unless sub_breed.empty?
+  end
+end
