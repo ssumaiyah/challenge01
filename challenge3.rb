99aa35968bdb2d1a7c951c7d3ce9e93b73f5370a
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-05-16 18:06:49 -0500

    updated challenge 3

--- challenge3.rb
+++ challenge3.rb
@@ -6,7 +6,7 @@ url = 'https://dog.ceo/api/breeds/list/all'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
- pp dog breeds
+
 
 # Access the hash containing the breeds and sub-breeds
 breeds_hash = dog_breeds['message']
